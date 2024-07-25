4b87698953f54428b8442767edd2d156dfdefcdf
gjvernaza <gjvernaza@gmail.com>
2024-07-24 22:15:07 -0500

    se hizo que se detenga unos puntos antes y se conecto direco al ESP32 además se se modificó las ganancias del controlador

--- seguimiento_camino.py
+++ seguimiento_camino.py
@@ -12,8 +12,8 @@ t = np.arange(0, tf+ts, ts)  # vector de tiempo
 N = len(t)  # longitud del vector de tiempo
 
 ############# Parámetros del robot #############
-a = 0.1063  # distancia del eje de las ruedas al centro del robot (vertical)
-b = 0.095  # distancia del eje de las ruedas al centro del robot (horizontal)
+a = 0.105  # distancia del eje de las ruedas al centro del robot (vertical)
+b = 0.105  # distancia del eje de las ruedas al centro del robot (horizontal)
 r = 0.04   # radio de las ruedas
 
 ############## Condiciones iniciales ############
@@ -25,13 +25,15 @@ hx[0] = 0  # posición en x
 hy[0] = 0  # posición en y
 theta[0] = 0*(np.pi/180)  # orientación inicial
 
-
 ############## Camino a seguir #############
-vMax = 0.2  # velocidad máxima del robot
+vMax = 0.3  # velocidad máxima del robot
 div = 3000
+# div = 4000
 # camino de 2 lineas
-ponits_x = [0, 0.86, .86, 1.72]
-ponits_y = [0, 0.86, 1.72, 1.72]
+ponits_x = [0, 0, 2.15, 2.15]  # puntos y
+ponits_y = [0, -0.86, -0.86, 0.86]  # puntos x
+# ponits_x = [0, 0]  # puntos x
+# ponits_y = [0, -2.06]  # puntos y
 
 px = []
 py = []
@@ -39,7 +41,6 @@ py = []
 for i in range(len(ponits_x)-1):
     px.append(np.linspace(ponits_x[i], ponits_x[i+1], div))
     py.append(np.linspace(ponits_y[i], ponits_y[i+1], div))
-
 
 pxd = np.hstack(px)
 pyd = np.hstack(py)
@@ -51,10 +52,10 @@ beta = np.zeros(sizePoints)  # angulo beta
 for i in range(sizePoints):
     if i == 0:
         beta[i] = np.arctan2(pyd[i+1]-pyd[i], pxd[i+1]-pxd[i])
-    else:        
+    else:
         beta[i] = np.arctan2(pyd[i]-pyd[i-1], pxd[i]-pxd[i-1])
 
-thetad = 0*(np.pi/180)*np.ones(N)  # orientación deseada
+thetad = -2*(np.pi/180)*np.ones(N)  # orientación deseada
 
 thetad_dot = np.zeros(N)  # velocidad angular deseada
 
@@ -74,7 +75,7 @@ hye = np.zeros(N)  # error en y
 thetae = np.zeros(N)  # error en orientación
 
 ########## Comunicación por socket ##########
-HOST = '192.168.1.50'  # Dirección IP del ESP32 configurado como AP
+HOST = '192.168.4.1'  # Dirección IP del ESP32 configurado como AP
 PORT = 80             # Puerto del servidor ESP32
 
 ######## Variables para guardar datos ########
@@ -92,7 +93,7 @@ vl = 0
 w = 0
 
 
-def send_data(sock, data: str):
+def send_data(sock: socket.socket, data: str):
     data = data + "\n"
     sock.sendall(data.encode())
 
@@ -107,10 +108,6 @@ def get_data(sock) -> list:
         vf = float(values[1])
         vl = float(values[3])
         w = float(values[5])
-    ##else:
-    ##    vf = 0
-    ##    vl = 0
-    ##    w = 0
 
     list_vf_med.append(vf)
     list_vl_med.append(vl)
@@ -121,89 +118,88 @@ def get_data(sock) -> list:
 
 
 # Crear el socket TCP/IP
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    try:
-        for k in range(N):
-            start_time = time.time()
-            
-            # punto mas cercano
-            min_dist = 1377.35
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+time.sleep(1)
+### Definir punto de parada con ts-time_slapsed
+##if len(ponits_x)>3:
+##    stop_index = sizePoints - 1225
+##else:
+##    stop_index = sizePoints - 210
 
-            for i in range(sizePoints):
-                dist = np.sqrt((pxd[i]-hx[k])**2 + (pyd[i]-hy[k])**2)
-                if dist < min_dist:
-                    min_dist = dist
-                    index = i
-            # Error
-            hxe[k] = pxd[index] - hx[k]
-            hye[k] = pyd[index] - hy[k]
-            thetae[k] = thetad[k] - theta[k]
+# Definir punto de parada con 
+# if len(ponits_x) > 3:
+#     stop_index = sizePoints - 1190
+# else:
+stop_index = sizePoints - 100
 
-            # Vector columna de errores 3x1
-            he = np.array([[hxe[k]], [hye[k]], [thetae[k]]])
 
-            # Matriz de Jacobiana
-            J = np.array([[np.cos(theta[k]), -np.sin(theta[k]), 0],
-                          [np.sin(theta[k]), np.cos(theta[k]), 0],
-                          [0, 0, 1]])
 
-            # Parámetros de control
-            # Parámetros de control
-            ##K = np.array([[2.15, 0, 0],
-            ##              [0, 2.1, 0],
-            ##              [0, 0, 2.22]])
+for k in range(N):
+    start_time = time.time()
+    # punto mas cercano
+    min_dist = 1.5
+    for i in range(sizePoints):
+        dist = np.sqrt((pxd[i]-hx[k])**2 + (pyd[i]-hy[k])**2)
+        if dist < min_dist:
+            min_dist = dist
+            index = i
+    # Verificar si el robot está en la última sección del camino y ha alcanzado el índice de parada
+    if index >= stop_index:            
+        break
+    # Error
+    hxe[k] = pxd[index] - hx[k]
+    hye[k] = pyd[index] - hy[k]
+    thetae[k] = thetad[k] - theta[k]
+    # Vector columna de errores 3x1
+    he = np.array([[hxe[k]], [hye[k]], [thetae[k]]])
+    # Matriz de Jacobiana
+    J = np.array([[np.cos(theta[k]), -np.sin(theta[k]), 0],
+                  [np.sin(theta[k]), np.cos(theta[k]), 0],
+                  [0, 0, 1]])
+    # Parámetros de control
+    ##K = 0.82*np.array([[1.2, 0, 0],  con ts-time_slapsed
+    ##                   [0, 1.8, 0],
+    ##                   [0, 0, 1.5]])
+    ##K = 0.82*np.array([[1.2, 0, 0], a Vmax = 0.26
+    ##                   [0, 1.35, 0],
+    ##                   [0, 0, 1.3]])
+    K = 0.82*np.array([[3.2, 0, 0],
+                       [0, 2.9, 0],
+                       [0, 0, 1.39]])
+    # Velocidades de referencia
+    pxdp = vMax * np.cos(beta[index])
+    pydp = vMax * np.sin(beta[index])
+    # Velocidades de referencia en coordenadas generalizadas (3x1)
+    pdp = np.array([[pxdp], [pydp], [thetad_dot[k]]])
+    # Ley de control
+    # velocidad de referencia en coordenadas generalizadas (3x1)
+    qp_ref = np.linalg.pinv(J) @ (pdp + K@he)
+    # Aplicar acciones de control
+    vf_ref[k] = qp_ref[0][0]
+    vl_ref[k] = qp_ref[1][0]
+    w_ref[k] = qp_ref[2][0]
+    # Enviar datos al robot
+    send_data(s, f"{vf_ref[k]},{vl_ref[k]},{w_ref[k]}")
+    vf_med[k], vl_med[k], w_med[k] = get_data(s)
+    # Integral numérica para calcular la orientación
+    theta[k + 1] = theta[k] + ts * w_med[k]
+    # Modelo cinemático
+    hxp = vf_med[k] * np.cos(theta[k + 1]) - \
+        vl_med[k] * np.sin(theta[k + 1])
+    hyp = vf_med[k] * np.sin(theta[k + 1]) + \
+        vl_med[k] * np.cos(theta[k + 1])
+    hx[k + 1] = hx[k] + ts * hxp
+    hy[k + 1] = hy[k] + ts * hyp
+    # Tiempo de muestreo
+    time_elapsed = time.time() - start_time
+    time.sleep( time_elapsed)
 
-            K = 0.87*np.array([[1.2, 0, 0],
-                          [0, 0.98, 0],
-                          [0, 0, 1.2]])
-
-            # Velocidades de referencia
-            pxdp = vMax * np.cos(beta[index])
-            pydp = vMax * np.sin(beta[index])
-
-            # Velocidades de referencia en coordenadas generalizadas (3x1)
-            pdp = np.array([[pxdp], [pydp], [thetad_dot[k]]])
-
-            # Ley de control
-            # velocidad de referencia en coordenadas generalizadas (3x1)
-            qp_ref = np.linalg.pinv(J) @ (pdp + K@he)
-
-            # Aplicar acciones de control
-            vf_ref[k] = qp_ref[0][0]
-            vl_ref[k] = qp_ref[1][0]
-            w_ref[k] = qp_ref[2][0]
-
-            # Enviar datos al robot
-            send_data(s, f"{vf_ref[k]},{vl_ref[k]},{w_ref[k]}")            
-            vf_med[k], vl_med[k], w_med[k] = get_data(s)
-
-            # Integral numérica para calcular la orientación
-            theta[k + 1] = theta[k] + ts * w_med[k]
-
-            # Modelo cinemático
-            hxp = vf_med[k] * np.cos(theta[k + 1]) - \
-                vl_med[k] * np.sin(theta[k + 1])
-            hyp = vf_med[k] * np.sin(theta[k + 1]) + \
-                vl_med[k] * np.cos(theta[k + 1])
-
-            hx[k + 1] = hx[k] + ts * hxp
-            hy[k + 1] = hy[k] + ts * hyp
-
-            # Tiempo de muestreo
-            time_elapsed = time.time() - start_time
-            time.sleep(time_elapsed)
-
-    finally:
-        # Enviar comando de detención
-        send_data(s, "0,0,0")
-        send_data(s, "0,0,0")
-        send_data(s, "0,0,0")
-        send_data(s, "0,0,0")
-
-    send_data(s, "0")
+for i in range(20):
     send_data(s, "0,0,0")
-    send_data(s, "0,0,0")
+    time.sleep(0.05)
+
+s.close()
 df.to_excel("datos_controlador.xlsx")
 
 plt.figure()
